e94ee9c7022dba8c8ac9c3989e3d1196d3a8041c
Damian S <damian.sevilla1@hotmail.com>
2023-10-04 08:54:39 -0600

    updates 3

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import logo from "../images/logo.png";
 import "./NavBar.css";
 
@@ -7,37 +7,35 @@ export class NavBar extends Component {
     return (
       <>
         <nav className="navbar-1">
-          <div className="nav-container">
-            <a className="" href="#dev-hero">
-              <img src={logo} alt="Web page logo" height="50" />
-            </a>
-            <ul className="nav-items">
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="#dev-hero"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#exp">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+          <a className="" href="#dev-hero">
+            <img src={logo} alt="Web page logo" height="50" />
+          </a>
+          <ul className="nav-items">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#dev-hero"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#exp">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
         </nav>
       </>
     );
